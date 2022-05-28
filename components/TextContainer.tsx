44d4dcb8712b6880c861a7552c9c73afdfe1c242
Manish Jha <manishjhazyx@gmail.com>
2022-05-28 22:59:50 +0530

    feat: added hero text

--- components/TextContainer.tsx
+++ components/TextContainer.tsx
@@ -1,5 +1,19 @@
 const TextContainer = () => {
-  return <div>TextContainer</div>;
+  return (
+    <div className="textContainer">
+      <div className="textContainer-top">
+        <span className="text-green-600">Sports</span>
+        <span className="text-yellow-600">Food</span>
+      </div>
+      <div className="textContainer-middle">
+        <span>GOOD NEWS</span>
+      </div>
+      <div className="textContainer-bottom">
+        <span className="text-cyan-600">Technology</span>
+        <span className="text-purple-600">Politics</span>
+      </div>
+    </div>
+  );
 };
 
 export default TextContainer;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import TextContainer from "../components/TextContainer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <main>
+        <TextContainer />
+      </main>
     </div>
   );
 }
